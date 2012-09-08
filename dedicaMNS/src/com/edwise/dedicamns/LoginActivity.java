e2b3e85e1e4a9c63b0d09fa093b4ed8a57c367fe
eantonc <eantonc@gmail.com>
2012-09-08 20:27:29 +0200

    Terminada pantalla de login, en modo mock. Añadido también soporte para otro idioma (inglés), pendiente de rellenar.

--- dedicaMNS/src/com/edwise/dedicamns/LoginActivity.java
+++ dedicaMNS/src/com/edwise/dedicamns/LoginActivity.java
@@ -2,21 +2,45 @@ package com.edwise.dedicamns;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.login);
+
+	Log.d(LoginActivity.class.toString(), "onCreate: Comenzando...");
+
+	// TODO obtener datos anteriores (preferences?)
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
+	getMenuInflater().inflate(R.menu.login, menu);
+	return true;
     }
 
-    
+    public void doLogin(View view) {
+	Log.d(LoginActivity.class.toString(), "doLogin: Click en conectar...");
+
+	EditText userLoginEditText = (EditText) findViewById(R.id.userMNText);
+	String userLogin = userLoginEditText.getText().toString();
+
+	EditText passLoginEditText = (EditText) findViewById(R.id.passMNText);
+	String passLogin = passLoginEditText.getText().toString();
+	
+	CheckBox rememberMeCheckBox = (CheckBox) findViewById(R.id.rememberUserCheck);
+	String checked = Boolean.toString(rememberMeCheckBox.isChecked());
+
+	Log.d(LoginActivity.class.toString(),
+		"doLogin: User y pass insertado: " + userLogin + " / "
+			+ passLogin + " / " + checked);
+    }
+
 }
