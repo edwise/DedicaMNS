4644ce062aba4aa9ab6fde0e180d4df6d2e20d7a
eantonc <eantonc@gmail.com>
2014-05-17 12:14:09 +0200

    Nueva versión. Cambio en mensajes de errores de conexión.

--- src/com/edwise/dedicamns/utils/ErrorUtils.java
+++ src/com/edwise/dedicamns/utils/ErrorUtils.java
@@ -10,19 +10,18 @@ import android.util.SparseArray;
  * 
  */
 public class ErrorUtils {
-
 	// TODO ojo! como meterlo como i18n?
 
 	private static final SparseArray<String> errorMessages = new SparseArray<String>();
 
 	static {
+		errorMessages.put(401, "Error en la conexión 401: revisa que tu usuario y password sean correctos");
+		errorMessages.put(404, "Error en la conexión 404: la web de dedicaciones parece caida...");
+		errorMessages.put(500, "Error en la conexión 500: la web está devolviendo un error 500...");
+
 		errorMessages.put(-1, "Sin conexión a internet: activa tu WIFI o conexión de datos");
-		errorMessages.put(401, "Error en la conexión: revisa que tu usuario y password sean correctos");
-		errorMessages.put(500, "Error en la conexión: la web está devolviendo un error 500...");
 		errorMessages.put(-2, "Error en la conexión: pete que flipas!");
-
 		errorMessages.put(-3, "Error en el formato de hora");
-
 		errorMessages.put(-4, "Error en el borrado de actividad diaria");
 	}
 
